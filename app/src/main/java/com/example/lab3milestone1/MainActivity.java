5fa6fe8c08bd0edb3ef384bea5a7becaee30e523
stormee7 <stormowski@wisc.edu>
2020-02-14 15:39:32 -0600

    first commit

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.lab3milestone1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog(){
+        DialogClass exampleDialog = new DialogClass();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
     }
 }
